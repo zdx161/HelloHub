01aafb8b82e6ac623e4e05a4fa355811dfa85d4a
cleverwo.999 <cleverwo.999@163.com>
2014-10-22 23:34:09 +0800

    generate pcm wav file: parse input option

--- createpcm.c
+++ createpcm.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <getopt.h>
 
 static void generate_sine(/*const snd_pcm_channel_area_t *areas,*/ 
 			  /*snd_pcm_uframes_t*/int offset,
@@ -95,13 +95,121 @@ static void generate_sine(/*const snd_pcm_channel_area_t *areas,*/
 }
 
 
+static void help(void)
+{
+    int k;
+    printf(
+        "Usage: createwav [OPTION]...\n"
+        "-h,--help	help\n"
+        "-r,--rate	stream rate in Hz\n"
+        "-c,--channels	count of channels in stream\n"
+        "-f,--frequency	sine wave frequency in Hz\n"
+        "-p,--period	period size in us\n"
+        "-o,--format	sample format\n"
+        "\n");
+#if 0
+    printf("Recognized sample formats are:");
+
+    for (k = 0; k < SND_PCM_FORMAT_LAST; ++k) {
+        const char *s = snd_pcm_format_name(k);
+
+        if (s)
+            printf(" %s", s);
+    }
+
+    printf("\n");
+    printf("Recognized transfer methods are:");
+
+    for (k = 0; transfer_methods[k].name; k++)
+        printf(" %s", transfer_methods[k].name);
+
+    printf("\n");
+#endif
+}
+
+static void print_config(int channels, int rate, int bps, int decibel)
+{
+    printf("====== show configuration info ======\n");
+    printf(" channels: %d ch\n", channels); 
+    printf(" rate: %d HZ\n", rate);
+    printf(" bps: %d bit/sample\n", bps);
+    printf(" decibel: %d db", decibel);
+    printf("================end====================\n");
+}
 //static void generate_sine(/*const snd_pcm_channel_area_t *areas,*/ 
 //			  snd_pcm_uframes_t offset,
 //			  int count, double *_phase, int channels, int freq, int format_bits, int rate)
-int main()
+//#define CREATE_NAME() 
+
+int main(int argc, char **argv)
 {
+    int channels = 2;
+    int rate = 44100;
+    int bps = 16;
+    int decibel = 0;
+    int freq = 440;
+    int period_time = 1000;
+
     double _phase = 8.4199314187571872e-13; 
-    generate_sine(0, 441000, &_phase, 1, 440, 16, 44100);
+
+	struct option long_option[] =
+	{
+		{"help", 0, NULL, 'h'},
+		{"rate", 1, NULL, 'r'},
+		{"channels", 1, NULL, 'c'},
+		{"frequency", 1, NULL, 'f'},
+		{"period", 1, NULL, 'p'},
+		{"format", 1, NULL, 'o'},
+		{NULL, 0, NULL, 0},
+	};
+
+	int morehelp = 0;
+	while (1) {
+		int c;
+		if ((c = getopt_long(argc, argv, "h:r:c:f:p:o", long_option, NULL)) < 0)
+			break;
+		switch (c) {
+		case 'h':
+			morehelp++;
+			break;
+		case 'r':
+			rate = atoi(optarg);
+			rate = rate < 4000 ? 4000 : rate;
+			rate = rate > 196000 ? 196000 : rate;
+			break;
+		case 'c':
+			channels = atoi(optarg);
+			channels = channels < 1 ? 1 : channels;
+			channels = channels > 1024 ? 1024 : channels;
+			break;
+		case 'f':
+			freq = atoi(optarg);
+			freq = freq < 50 ? 50 : freq;
+			freq = freq > 5000 ? 5000 : freq;
+			break;
+		case 'p':
+			period_time = atoi(optarg);
+			period_time = period_time < 1000 ? 1000 : period_time;
+			period_time = period_time > 1000000 ? 1000000 : period_time;
+			break;
+		case 'o':
+            printf("served set config item\n");
+			break;
+        defaddult:
+            printf("using default configuration:\n");
+            break;
+		}
+	}
+
+	if (morehelp) {
+		help();
+		return 0;
+	}
+    
+    print_config(channels, channels, rate, decibel);
+
+    //double _phase = 8.4199314187571872e-13; 
+    //generate_sine(0, 441000, &_phase, 1, 440, 16, 44100);
   
     return 0;
 }

6de29bf2a3ec86cd1e1831a25a48c9ca342c2815
zedong.xiong <zedong.xiong@amlogic.com>
2015-01-23 19:42:15 +0800

    algorithm study: merge OK

--- algorithm/merge.c
+++ algorithm/merge.c
@@ -2,29 +2,36 @@
 #include <stdlib.h>
 
 
-void merge(int *data, int p, int q, int r)
+void merge(int * const data, int p, int q, int r)
 {
     int i,j,k;
-    int n1 = q - p + 1;
-    int n2 = r - q;
+    int n1 = q - p;
+    int n2 = r - q + 1;
     int *d1 = (int *)calloc(1, n1*sizeof(int));
     int *d2 = (int *)calloc(1, n2*sizeof(int));
-    memcpy(d1, data, n1*sizeof(int));
-    memcpy(d2, data+(n1*sizeof(int)), n2*sizeof(int));
+    for(i = 0; i < n1; i++){
+        d1[i] = data[i];
+    }
+    for(i = 0; i < n2; i++){
+        d2[i] = data[n1+i];
+    }
     
     j = k =0;
+
     for(i = 0; i < n1 + n2; i++){
         if(j >= n1 && k < n2){
             data[i] = d2[k];
+            k++;
         }
         else if(k >= n2 && j < n1){
             data[i] = d1[j];
+            j++;
         }
         else if(d1[j] <= d2[k]){
             data[i] = d1[j];
             j++;
         }else{
-            data[i] = d2[j];
+            data[i] = d2[k];
             k++;
         }
     }
@@ -33,23 +40,28 @@ void merge(int *data, int p, int q, int r)
     free(d2);
 }
 
-void merge_sort(int *data, int p, int q)
+void merge_sort(int * const data, int p, int q)
 {
-    int mid = (p + q) / 2;
-    if(q - p > 2){
+    int mid = (p + q + 1) / 2;
+    if(q > p){
         merge_sort(data, p, mid -1);
-        merge_sort(data+mid, mid, q);
-    }else{
+        merge_sort((data + (mid - p)), mid, q);
         merge(data, p, mid, q);
+    }else{
+       //do nothing
+       return ;
+       // merge(data, p, mid, q);
     }
 }
 
 int main(){
-    int d1[5] = {2,5,1,3,0,4,6,9,7,8};
-    merge_sort(d1, 0, 9);
+    int d1[10] = {2,5,1,3,0,4,6,9,7,8};
+    //int d1[5] = {2,5,1,3,0};
+    int size = 10;
+    merge_sort(d1, 0, size - 1);
 
     int i;
-    for(i = 0; i < 10; i++){
+    for(i = 0; i < size; i++){
         printf("%d, ", d1[i]);
     }
     printf("\n");

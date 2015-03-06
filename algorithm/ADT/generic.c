d076916dbf5337bf81bb2df205d5b27b3033b27b
cleverwo.999 <cleverwo.999@163.com>
2015-03-06 23:49:17 +0800

    ADT: using macro to implement generic function

---
+++ algorithm/ADT/generic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "generic.h"
+
+
+GENERIC_STACK(int, _int, 10)
+GENERIC_STACK(float, _float, 5)
+
+int main()
+{
+    push_int(5);
+    push_int(22);
+    push_int(15);
+    push_float(25.3);
+    push_float(-40.5);
+
+    while (!is_empty_int()) {
+        printf("Popping %d\n", top_int());
+        pop_int();
+    }
+
+    while (!is_empty_float()) {
+        printf("Popping %f\n", top_float());
+        pop_float();
+    }
+
+    return EXIT_SUCCESS;
+}
